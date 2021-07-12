fb1f41c6c428a6da156502d14ded15b34b566b09
Arthur Bouton <arthur.bouton@gadz.org>
2021-07-12 18:00:03 +0100

    Compensate SEA non-linearity in torque control in crawling_gait.py

--- RaspberryPi/rover_ctrl/scripts/crawling_gait.py
+++ RaspberryPi/rover_ctrl/scripts/crawling_gait.py
@@ -104,6 +104,10 @@ def get_desired_torque( rate_sign ) :
 
 	desired_torque = torque_middle - ( torque_middle - torque_edge )*abs( actual_angle + rate_sign*torque_peak )/( angle_amplitude - peak_sign*torque_peak )
 
+	# Compensate the non-linearity:
+	if desired_torque > 0 :
+		desired_torque *= 1.2
+
 	return -rate_sign*desired_torque
 
 
